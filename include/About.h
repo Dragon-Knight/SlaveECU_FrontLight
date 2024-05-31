a8e91a5306d222b4bc9a607b5cc078804f6f3b67
GrKoR <dr.g.kornev@gmail.com>
2024-05-31 18:32:19 +0300

    readme updated, board_ver++

--- include/About.h
+++ include/About.h
@@ -5,7 +5,7 @@ namespace About
 	static constexpr char name[] = "FrontLightECU";
 	static constexpr char desc[] = "Front light control board for Pixel project";
 	static constexpr char board_type = Consts::BOARD_TYPE_FRONTLIGHT;		// 5 bits
-	static constexpr char board_ver = 1;		// 3 bits
+	static constexpr char board_ver = 2;		// 3 bits
 	static constexpr char soft_ver = 1;			// 6 bits
 	static constexpr char can_ver = 1;			// 2 bits
 	static constexpr char git[] = "https://github.com/starfactorypixel/SlaveECU_FrontLight";
